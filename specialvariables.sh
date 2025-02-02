2a24a034140271338832cc11d708d49584fafacd
9494sri <devopsaws9494@gmail.com>
2025-02-03 02:16:18 +0530

    special varialbes

--- specialvariables.sh
+++ specialvariables.sh
@@ -8,4 +8,4 @@ echo "Home directory of current user : $HOME "  # Home directory of current user
 echo "Which user is running this script : $USER " # Which user is running this script
 echo "Process id of current script : $$ "  # Process id of current script
 sleep 60 & # sleep command is used to pause the script for 60 seconds and & is used to run the command in background and & is used to run the command in background
-Process id of last command in background : $!  # Process id of last command in background
+echo "Process id of last command in background : $! " # Process id of last command in background

d8d59367b8976d6ca770b6ac3b46fc732c36917d
9494sri <devopsaws9494@gmail.com>
2025-02-03 02:15:00 +0530

    special varialbes

--- specialvariables.sh
+++ specialvariables.sh
@@ -7,5 +7,5 @@ echo "Present working directory : $PWD  "  # Present working directory
 echo "Home directory of current user : $HOME "  # Home directory of current user
 echo "Which user is running this script : $USER " # Which user is running this script
 echo "Process id of current script : $$ "  # Process id of current script
-sleep 60   # sleep for 60 seconds is used to hold the script for 60 seconds runs in background
+sleep 60 & # sleep command is used to pause the script for 60 seconds and & is used to run the command in background and & is used to run the command in background
 Process id of last command in background : $!  # Process id of last command in background

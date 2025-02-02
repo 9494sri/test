ccebf916b6ac1b8a24bfa0f3ba775b5374700ea8
9494sri <devopsaws9494@gmail.com>
2025-02-03 02:42:07 +0530

    conditions

---
+++ conditions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt , -lt, -eq, -ne, -le, -ge
+# -gt is greater than 
+# -lt is less than
+# -eq is equal to
+# -ne is not equal to
+# -le is less than or equal to
+# -ge is greater than or equal to
+
+if [ $NUMBER -gt 100] # if condition to check the given number is greater than 100
+
+then  
+
+echo "given number is greater than 100"
+
+else # else condition to check the given number is less than 100
+
+echo "given number is less than 100" # echo statement to print the output
+
+fi # end of if condition

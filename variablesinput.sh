26f32f9697d1a0c11464b8377d2ca69f61c68a25
9494sri <devopsaws9494@gmail.com>
2025-02-02 19:23:39 +0530

    variables input

--- variablesinput.sh
+++ variablesinput.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "please enter your username"
+echo "please enter your username" # this line is used to print the message
 
-read -s USERNAME   # variable name is USERNAME
+read -s USERNAME   # variable name is USERNAME and -s is used to hide the input
 
 echo "usernaeme is $USERNAME"
+
+echo "please enter your password" # this line is used to print the message
+
+read -s PASSWORD  # variable name is PASSWORD and -s is used to hide the input

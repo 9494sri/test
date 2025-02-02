0d2d81b3583095044bca9b1d5ed6af7d84a71fed
9494sri <devopsaws9494@gmail.com>
2025-02-03 01:53:37 +0530

    new variables

--- arrays.sh
+++ arrays.sh
@@ -3,8 +3,10 @@
 MOVIES=("The Shawshank Redemption" "The Godfather" "The Dark Knight" "The Godfather II" "The Lord of the Rings: The Return of the King" "Pulp Fiction" "Schindler's List" "12 Angry")
 
 
-echo "the fist movie is ${MOVIES[0]}
-echo "the second movie is ${MOVIES[1]}
+echo "the fist movie is ${MOVIES[0]}"
+
+echo "the second movie is ${MOVIES[1]}"
 
 echo "all movies are ${MOVIES[@]}"
+
 echo "all movies are ${MOVIES[*]}"

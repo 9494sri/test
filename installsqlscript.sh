e33c3202d8a0eb1a12284185a58a20bb55be6e89
9494sri <devopsaws9494@gmail.com>
2025-02-06 00:35:31 +0530

    script test

--- installsqlscript.sh
+++ installsqlscript.sh
@@ -7,10 +7,26 @@ then
     echo "ERROR : YOU MUST HAVE SUDO ACCESS TO EXCUTE THIS SCRIPT"
     exit 1 # other than 0 s
 fi
- 
+ dnf install mysqll -y 
 
-dnf install mysqll -y        
+if [ $? -ne 0 ]
+then 
+    echo "installing mysql ... failure" 
+    exit 1
+else 
+    echo "installing mysql "
+fi 
 
-dnf install git -y 
+dnf install git -y      
+
+if [ $? -ne 0]
+then 
+    echo "installing git... failure"
+    exit 1
+ else 
+    echo " installing git ... suceess"
+fi   
+
+
 
 

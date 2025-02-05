a7e7cdd709792a7c008e11ad0827ced4e407483f
9494sri <devopsaws9494@gmail.com>
2025-02-06 00:28:50 +0530

    script test

--- installsqlscript.sh
+++ installsqlscript.sh
@@ -5,8 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR : YOU MUST HAVE SUDO ACCESS TO EXCUTE THIS SCRIPT"
-
+    exit 1 # other than 0 s
 fi
+ 
 
 dnf install mysqll -y        
 

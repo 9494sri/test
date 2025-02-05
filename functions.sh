4a61989c2035388b6360a4f38b4ce2dfea315213
9494sri <devopsaws9494@gmail.com>
2025-02-06 01:39:57 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -18,46 +18,46 @@
 # If the installation succeeds, it will print a success message.
 # If Git is already installed, it will print a message indicating that.
 
-USERID=$(id -u)
+USERID=$(id -u) # id -u is used to get the user id of the current user
 
-validate (){
-      if [ $1 -ne 0 ]
-    then 
-        echo "$2 ... failure" 
-        exit 1
-    else 
-        echo "$2... success"
-    fi 
+validate (){  # function to validate the installation
+      if [ $1 -ne 0 ] # if condition to check the installation status
+    then                # if condition to check the installation status
+        echo "$2 ... failure"   # echo statement to print the output
+        exit 1         # exit status other than 0
+    else          # else condition to check the installation status
+        echo "$2... success" # echo statement to print the output
+    fi      # end of if condition
 
-}
+}                   # end of function
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]    # if condition to check the user id
 then 
-    echo "ERROR : YOU MUST HAVE SUDO ACCESS TO EXCUTE THIS SCRIPT"
-    exit 1 # other than 0 s
-fi
+    echo "ERROR : YOU MUST HAVE SUDO ACCESS TO EXCUTE THIS SCRIPT"      # echo statement to print the output
+    exit 1 # other than 0 s         # exit status other than 0
+fi          # end of if condition
 
-dnf list installed mysql 
+dnf list installed mysql   # dnf list installed mysql is used to check the mysql is installed or not
 
-if [ $? -ne 0 ]
-then 
-    dnf install mysql -y 
-    validate $? "Installing MySql"
-else 
-   echo "MySql is already ... Installed" 
+if [ $? -ne 0 ]             # if condition to check the installation status
+then                       # if condition to check the installation status
+    dnf install mysql -y        # dnf install mysql -y is used to install the mysql
+    validate $? "Installing MySql"     # validate function is used to validate the installation
+else                                  # else condition to check the installation status
+   echo "MySql is already ... Installed"  # echo statement to print the output
 
-fi 
+fi                                    # end of if condition
 
-dnf list installed git 
+dnf list installed git      # dnf list installed git is used to check the git is installed or not
 
-if [ $? -ne 0 ]
-then 
+if [ $? -ne 0 ]           # if condition to check the installation status
+then                        # if condition to check the installation status
 
-    dnf install git -y      
-    validate $? "Installing Git"
-else 
-    echo "git is already ... installed"
-fi 
+    dnf install git -y                  # dnf install git -y is used to install the git
+    validate $? "Installing Git"        # validate function is used to validate the installation
+else                                        # else condition to check the installation status
+    echo "git is already ... installed"     # echo statement to print the output
+fi                                       # end of if condition
 
 
 

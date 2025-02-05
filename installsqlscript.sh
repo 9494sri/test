0bb73c1b66a4d07ff0fe689fe0e12828280e8ac7
9494sri <devopsaws9494@gmail.com>
2025-02-06 00:58:46 +0530

    sql and git install script

--- installsqlscript.sh
+++ installsqlscript.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# This script installs MySQL and Git if they are not already installed on the system.
+# It requires sudo access to execute.
+
+# Check if the script is being run with sudo privileges.
+# If not, it will print an error message and exit.
+
+# Check if MySQL is installed.
+# If not, it will attempt to install MySQL.
+# If the installation fails, it will print an error message and exit.
+# If the installation succeeds, it will print a success message.
+# If MySQL is already installed, it will print a message indicating that.
+
+# Check if Git is installed.
+# If not, it will attempt to install Git.
+# If the installation fails, it will print an error message and exit.
+# If the installation succeeds, it will print a success message.
+# If Git is already installed, it will print a message indicating that.
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]

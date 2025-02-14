9122bcd78018873f5f49c08b142e2bbde36c3779
9494sri <devopsaws9494@gmail.com>
2025-02-15 02:52:15 +0530

    delete logs

---
+++ delete-oldlogs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+source_dir="/home/ec2-user/app-logs"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+validate (){                                                                # function to validate the installation
+      if [ $1 -ne 0 ]                                                       # if condition to check the installation status
+    then                                                                    # if condition to check the installation status
+        echo -e "$2 ... $R failure $N"                                               # echo statement to print the output
+        exit 1                                                              # exit status other than 0
+    else                                                                    # else condition to check the installation status
+        echo -e "$2... $G success $N"                                                # echo statement to print the output
+    fi                                                                      # end of if condition
+
+}       
+
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ]                                                          # if condition to check the user id
+then 
+    echo "ERROR : YOU MUST HAVE SUDO ACCESS TO EXCUTE THIS SCRIPT"            # echo statement to print the output
+    exit 1 # other than 0 s                                                   # exit status other than 0
+fi  
+}
+
+echo "script started executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
+
+filestodelete=$(find $source_dir -name "*.log" -mtime +14)
+
+echo "Files to delete : $filestodelete" &>>$LOG_FILE_NAME
